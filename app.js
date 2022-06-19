71c7d436b740e4f85c89d46c526a7c5702664ed1
Miskevich Stanislav <miskevich95@yandex.ru>
2022-06-19 21:32:06 +0300

    The first is a test task+

---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require('express');
+
+const { PORT = 3000 } = process.env;
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
+const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+mongoose.connect('mongodb://localhost:27017/mestodb');
+
+
+app.use((req, res, next) => {
+  req.user = {
+    _id: '62af6221b687abfb13dc1c2e',
+  };
+  next();
+});
+
+app.use('/', require('./routes/users'));
+app.use('/', require('./routes/cards'));
+
+app.use('*', (_req, res) => {
+  res.status(404).send({ message: 'Что-то пошло не так!' });
+});
+
+app.listen(PORT,);
