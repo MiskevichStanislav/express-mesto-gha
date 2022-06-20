37f9b9e3ef620ff4b39b1635d9f92fc2f94340a0
Miskevich Stanislav <miskevich95@yandex.ru>
2022-06-20 12:48:24 +0300

    ++

--- app.js
+++ app.js
@@ -1,6 +1,15 @@
 const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 const app = express();
-const PORT = 3000;
+mongoose.connect('mongodb://localhost:27017/mestodb')
+  .catch((err) => {
+    console.log(err);
+  });
 
+const { PORT = 3000 } = process.env;
 
-app.listen(PORT);
+app.listen(3000, () => {
+  console.log('Hura!');
+});
