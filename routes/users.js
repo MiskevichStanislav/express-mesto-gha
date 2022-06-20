a74ac6e6aa46d605955a4ee7a8412178c1d6386b
Miskevich Stanislav <miskevich95@yandex.ru>
2022-06-20 14:56:21 +0300

    ++

--- routes/users.js
+++ routes/users.js
@@ -12,4 +12,5 @@ router.get('/users/:userId', getUser);
 router.post('/users', createUser);
 router.patch('/users/me', updateUser);
 router.patch('/users/me/avatar', updateAvatar);
+
 module.exports = router;
