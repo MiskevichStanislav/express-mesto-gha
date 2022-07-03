793cfa82ef329c1c7f64ce4482a1fa847ed3ce1b
Miskevich Stanislav <miskevich95@yandex.ru>
2022-07-03 13:57:48 +0300

    +

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -2,22 +2,22 @@ const jwt = require('jsonwebtoken');
 const AuthorisationError = require('../errors/AuthorisationErr_401');
 
 module.exports = (req, res, next) => {
-    const { authorization } = req.headers;
+  const { authorization } = req.headers;
 
-    if (!authorization || !authorization.startsWith('Bearer')) {
-        return next(new AuthorisationError('Необходима авторизация'));
-    }
+  if (!authorization || !authorization.startsWith('Bearer')) {
+    return next(new AuthorisationError('Необходима авторизация'));
+  }
 
-    const token = authorization.replace('Bearer ', '');
-    let payload;
+  const token = authorization.replace('Bearer ', '');
+  let payload;
 
-    try {
-        payload = jwt.verify(token, 'some-secret-key');
-    } catch (err) {
-        throw new AuthorisationError('Необходима авторизация');
-    }
+  try {
+    payload = jwt.verify(token, 'some-secret-key');
+  } catch (err) {
+    throw new AuthorisationError('Необходима авторизация');
+  }
 
-    req.user = payload;
+  req.user = payload;
 
-    return next();
+  return next();
 };
