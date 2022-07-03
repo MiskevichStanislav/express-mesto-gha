f7c7c0a8c8bebdbd43db37201c4d0a71ba988a26
Miskevich Stanislav <miskevich95@yandex.ru>
2022-07-03 13:22:32 +0300

    +

--- app.js
+++ app.js
@@ -3,8 +3,11 @@ const express = require('express');
 const { PORT = 3000 } = process.env;
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const { celebrate, Joi, errors } = require('celebrate');
 const { login, createUser } = require('./controllers/users');
 const auth = require('./middlewares/auth');
+const NotFoundError = require('./errors/NotFoundErr_404');
+
 const app = express();
 
 mongoose.connect('mongodb://localhost:27017/mestodb');
@@ -12,22 +15,40 @@ mongoose.connect('mongodb://localhost:27017/mestodb');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use((req, res, next) => {
-  req.user = {
-    _id: '62b04915aec6cca2298e1a36',
-  };
+app.post('/signup', celebrate({
+  body: Joi.object().keys({
+    name: Joi.string().min(2).max(30),
+    about: Joi.string().min(2).max(30),
+    avatar: Joi.string().regex(/^(https?:\/\/(www\.)?([a-zA-z0-9-]){1,}\.?([a-zA-z0-9]){2,8}(\/?([a-zA-z0-9-])*\/?)*\/?([-._~:/?#[]@!\$&'\(\)\*\+,;=])*)/),
+    email: Joi.string().required().email(),
+    password: Joi.string().required(),
+  }),
+}), createUser);
+app.post('/signin', celebrate({
+  body: Joi.object().keys({
+    email: Joi.string().required().email(),
+    password: Joi.string().required(),
+  }),
+}), login);
 
-  next();
-});
-
-app.post('/signin', login);
-app.post('/signup', createUser);
 app.use(auth);
 app.use('/', require('./routes/users'));
 app.use('/', require('./routes/cards'));
 
-app.use('*', (_req, res) => {
-  res.status(404).send({ message: 'Что-то пошло не так попробуйте перезагрузить страницу' });
+app.use('*', (req, res, next) => {
+  next(new NotFoundError('Страница не найдена'));
+});
+app.use(errors());
+app.use((err, req, res, next) => {
+  const { statusCode = 500, message } = err;
+  res
+    .status(err.statusCode)
+    .send({
+      message: statusCode === 500
+        ? 'Произошла ошибка на сервере'
+        : message,
+    });
+  next();
 });
 
 app.listen(PORT);
