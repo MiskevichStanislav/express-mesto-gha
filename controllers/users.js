a1c068903d2d9dadc0c256855caac930c47f4cf1
Miskevich Stanislav <miskevich95@yandex.ru>
2022-07-03 12:50:58 +0300

    editing a module user+

--- controllers/users.js
+++ controllers/users.js
@@ -1,69 +1,91 @@
+const validator = require('validator');
+const bcrypt = require('bcryptjs');
 const User = require('../models/user');
+const AuthError = require('../errors/authorisation_error_401');
+const ConflictError = require('../errors/conflict_409');
+const NotFoundError = require('../errors/not-found-err_404');
+const ValidError = require('../errors/validation_error_400');
 
-module.exports.getUsers = (req, res) => {
+module.exports.getUsers = (req, res, next) => {
   User.find({})
     .then((users) => res.send({ data: users }))
-    .catch(() => res.status(500).send({ message: 'Ошибка' }));
+    .catch(next);
 };
-module.exports.getUser = (req, res) => {
+module.exports.getUser = (req, res, next) => {
   User.findById(req.params.userId)
     .then((user) => {
-      if (user === null) {
-        return res
-          .status(404)
-          .send({ message: 'Пользователь по указанному Id не найден.' });
+      if (!user) {
+        return next(new NotFoundError('Пользователь по указанному Id не найден.'));
       }
       return res.send({ data: user });
     })
-    .catch((err) => {
-      if (err.name === 'CastError') {
-        return res.status(400).send({ message: 'Передан некорректный Id' });
-      }
-      return res.status(500).send({ message: 'Ошибка' });
-    });
+    .catch(next);
 };
 module.exports.createUser = (req, res) => {
-  const { name, about, avatar } = req.body;
+  const { name, about, avatar, email, password } = req.body;
 
-  User.create({ name, about, avatar })
-    .then((user) => res.send({ data: user }))
+  bcrypt.hash(password, 10)
+    .then((hash) => User.create({
+      name,
+      about,
+      avatar,
+      email,
+      password: hash,
+    }))
+    .then((user) => res.status(201).send({
+      name: user.name,
+      about: user.about,
+      avatar: user.avatar,
+      email: user.email,
+      _id: user._id,
+    }))
     .catch((err) => {
       if (err.name === 'ValidationError') {
-        return res
-          .status(400)
-          .send({ message: 'Переданы некорректные данные при создании user' });
+        next(new ValidError('Переданы некорректные данные при создании user'));
+      } else if (err.code === 11000) {
+        next(new ConflictError('Такой пользователь уже существует!)'));
+      } else {
+        next(err);
       }
-      return res.status(500).send({ message: 'Ошибка' });
     });
 };
-module.exports.updateUser = (req, res) => {
+module.exports.updateUser = (req, res, next) => {
   const { name, about } = req.body;
   User.findByIdAndUpdate(req.user._id, { name, about }, {
     new: true,
     runValidators: true,
   })
-    .then((user) => res.status(200).send(user))
-    .catch((error) => {
-      if (error.name === 'ValidationError') {
-        res.status(400).send({ message: 'Некорректные данные при обновлении профиля' });
-        return;
+    .then((user) => {
+      if (!user) {
+        throw new NotFoundError('Пользователь по указанному _id не найден');
       }
-      res.status(500).send({ message: 'Ошибка' });
+      res.send(user);
+    })
+    .catch((err) => {
+      if (err.name === 'ValidationError') {
+        return next(new ValidError('Введены некоретные данные'));
+      }
+      return next(err);
     });
 };
 
-module.exports.updateAvatar = (req, res) => {
+module.exports.updateAvatar = (req, res, next) => {
   const { avatar } = req.body;
   User.findByIdAndUpdate(req.user._id, { avatar }, {
     new: true,
     runValidators: true,
   })
-    .then((user) => res.status(200).send(user))
-    .catch((error) => {
-      if (error.name === 'ValidationError') {
-        res.status(400).send({ message: 'Некорректные данные при обновлении аватара' });
-        return;
+    .then((user) => {
+      if (!user) {
+        throw new NotFoundError('Пользователь по указанному _id не найден');
       }
-      res.status(500).send({ message: 'Ошибка' });
+      res.send(user);
+    })
+
+    .catch((err) => {
+      if (err.name === 'ValidationError') {
+        return next(new ValidError('Введены некоректные данные'));
+      }
+      return next(err);
     });
 };
